22174a1137be8245cda9865a228e531d1e8789ca
eruku iida <iida@erukunoMacBook-Pro.local>
2022-04-15 13:48:23 +0900

    change contents of html

--- hello_gin/main.go
+++ hello_gin/main.go
@@ -2,13 +2,25 @@ package main
 import(
 	"log"
 	"github.com/gin-gonic/gin"
+	//"net/http"
 )
 
 func main(){
 	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	ua := ""
+
+	router.Use(func(c *gin.Context){
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
 
 	router.GET("/",func(ctx *gin.Context){
-		ctx.String(200,"Hello, World!")
+		ctx.HTML(200,"index.html",gin.H{
+			"message": "hello world",
+			"User-Agent": ua,
+		})
+		
 	})
 
 	err :=router.Run("127.0.0.1:8888")
