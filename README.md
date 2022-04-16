3e7a65a5580b4eff63da34a3bcc396835e2a23c8
eruku iida <iida@erukunoMacBook-Pro.local>
2022-04-16 21:10:09 +0900

    change Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # web_test
+
+front vue(javascript)
+
+back gin(golang)
+
+css semantic
+
+でお試ししている所
--- hello_gin/main.go
+++ hello_gin/main.go
@@ -2,29 +2,69 @@ package main
 import(
 	"log"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
 	//"net/http"
 )
+
+type Todo struct {
+	gorm.Model
+	Memo string
+}
 
 func main(){
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+	dbInit()
 	ua := ""
 
-	router.Use(func(c *gin.Context){
-		ua = c.GetHeader("User-Agent")
-		c.Next()
-	})
-
-	router.GET("/",func(ctx *gin.Context){
-		ctx.HTML(200,"index.html",gin.H{
-			"message": "hello world",
-			"User-Agent": ua,
-		})
-		
-	})
+	router.GET("/",getHandler)
+	r.POST("/new",postHandler)
+	r.Run()
 
 	err :=router.Run("127.0.0.1:8888")
 	if err != nil{
 		log.Fatal("fail",err)
 	}
+}
+
+
+func getHandler(c *gin.Context){
+	todo := getAll()
+	c.HTML(200,"index.html",gin.H{"todo":todo})
+}
+
+func postHandler(c *gin.Context){
+	memo := c.postFrom("memo")
+	create(memo)
+	c.Redirect(302,"/")
+}
+
+func dbInit(){
+	db,err := gorm.Open("sqlite3","todo.splite3")
+	if err != nul {
+
+		panic("fail to connect db\n")
+	}
+}
+
+
+func create (memo string){
+	db,err := gorm.Open("sqlite3","todo.splite3")
+	if err != nul {
+
+		panic("fail to connect db\n")
+	}
+	db.Create(&Todo(Memo:memo))
+}
+
+func getAll() []Todo{
+	db,err = gorm.Open("sqlite3","todo.splite3")
+	if err != nul {
+
+		panic("fail to connect db\n")
+	}
+	var todo []Todo
+	db.Find(&todo)
+	return todo
 }
